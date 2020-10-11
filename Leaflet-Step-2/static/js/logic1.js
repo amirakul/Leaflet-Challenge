bfce0526b436aad7cf8471f8243e8bccfb765b52
Jildiz Amirakul <jildiz@Jildizs-MacBook-Pro.local>
2020-10-10 20:44:35 -0500

    updated styles

--- Leaflet-Step-2/static/js/logic1.js
+++ Leaflet-Step-2/static/js/logic1.js
@@ -22,12 +22,6 @@ var greyscale =  L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles
     accessToken: API_KEY
 });
 
-// L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-//     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-//     maxZoom: 18,
-//     id: "mapbox.light",
-//     accessToken: API_KEY
-// });
 
 var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
